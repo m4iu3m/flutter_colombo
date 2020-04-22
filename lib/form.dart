ac719435e19f6aabd32bdd70ce02c3e46f19bdbf
m4iu3m <m4iu3m@gmail.com>
2020-04-22 09:49:26 +0700

    fix

--- lib/form.dart
+++ lib/form.dart
@@ -7,6 +7,3 @@ export 'form/form_group.dart';
 export 'form/form_date_picker.dart';
 export 'form/form_select_region_address.dart';
 export 'form/form_show_checked.dart';
-class Test{
-  print('test');
-}
