7b80037d70fc375d994e96951f982cfeda6a55c7
m4iu3m <m4iu3m@gmail.com>
2020-04-23 15:27:11 +0700

    fix select

--- lib/form/form_show_checked.dart
+++ lib/form/form_show_checked.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'form_checkbox.dart';
+import 'package:flutter_colombo/form/form_checkbox.dart';
 
 // ignore: must_be_immutable
 class FormShowChecked extends StatefulWidget {
